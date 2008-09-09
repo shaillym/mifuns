6f45355dbb29b6cdd133626752aaa7270eac5919
bergsmat <bergsmat@8d4d5234-0756-0410-9cbe-b5ace3ed5f0a>
2008-09-09 15:06:04 +0000

    doc fixes

--- trunk/R/Ops.miDateTime.R
+++ trunk/R/Ops.miDateTime.R
@@ -1,7 +1,7 @@
 `Ops.miDateTime` <-
 function(e1,e2){
-	f1 <- as.miDateTime(as.character(e1),format = "%m/%d/%Y %H:%M")
-	f2 <- as.miDateTime(as.character(e2),format = "%m/%d/%Y %H:%M")
+	f1 <- as.miDateTime(as.character(e1))
+	f2 <- as.miDateTime(as.character(e2))
 	e1[!is.na(f1)] <- f1[!is.na(f1)]
 	e2[!is.na(f2)] <- f2[!is.na(f2)]
 	NextMethod()
--- trunk/R/Ops.miTime.R
+++ trunk/R/Ops.miTime.R
@@ -1,7 +1,7 @@
 `Ops.miTime` <-
 function(e1,e2){
-	f1 <- as.miTime(as.character(e1),format = "%H:%M")
-	f2 <- as.miTime(as.character(e2),format = "%H:%M")
+	f1 <- as.miTime(as.character(e1))
+	f2 <- as.miTime(as.character(e2))
 	e1[!is.na(f1)] <- f1[!is.na(f1)]
 	e2[!is.na(f2)] <- f2[!is.na(f2)]
 	NextMethod()

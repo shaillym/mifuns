98475ef6d5aaa0028d954a861fdd11be6d0ee323
bergsmat <bergsmat@8d4d5234-0756-0410-9cbe-b5ace3ed5f0a>
2008-10-21 14:37:23 +0000

    revised temporals

--- R/format.miDateTime.R
+++ R/format.miDateTime.R
@@ -3,6 +3,7 @@ function(x,...){
 x <- as.numeric(x)#coerce to numeric
 dt <- trunc(x)#capture the date part
 tm <- x %% 1#capture the time part
-paste(format(as.miDate(dt)),format(as.miTime(tm)))
+y <- paste(format(as.miDate(dt)),format(as.miTime(tm)))
+y[is.infinite(x)] <- x[is.infinite(x)]
 }
 
--- R/format.miTime.R
+++ R/format.miTime.R
@@ -7,6 +7,7 @@ mn <- mn %% 60			#remaining minutes
 join <- sprintf("%02.0f:%02.0f",hr,mn)
 #hours and mins printed as floats with 2 digits, padding with zeros as nec. (no decimal printed, sep by colon).
 join[is.na(x)] <- NA#restore NA's.  'NA' looks better than "NA:NA".
+join[is.infinite(x)] <- x[is.infinite(x)]#restore Inf, -Inf.
 join
 }
 

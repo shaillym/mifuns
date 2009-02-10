3030326deb51c30eb822f63fc769a57f20018b6b
bergsmat <bergsmat@8d4d5234-0756-0410-9cbe-b5ace3ed5f0a>
2009-02-10 17:52:17 +0000

    3.2.10

--- trunk/R/constant.R
+++ trunk/R/constant.R
@@ -1,2 +1,9 @@
-`constant` <-function(x,within)x %nests% within
-
+constant <- function(x,...)UseMethod('constant')
+constant.default <- function(x,within,...)x%nests%within
+constant.data.frame <- function(x,within=NULL,...){
+	if(!is.null(within))if(!is.character(within) | !all(within %in% names(x)))stop('within must be a character vector of names in x')
+	if (is.null(within))index <- rep(1,nrow(x))
+	else index <- do.call(paste,x[,within,drop=FALSE])
+	cons <- sapply(x,constant,within=index)
+	unique(x[, names(x)[cons],...])
+}	

b9baebeb0f7e8d0894e0946ef2fb22d342fbe04f
billkneb <billkneb@8d4d5234-0756-0410-9cbe-b5ace3ed5f0a>
2009-03-23 19:08:57 +0000

    fixed runnm.R and NONR.R

--- branches/timb-metrumrg-com/R/NONR.R
+++ branches/timb-metrumrg-com/R/NONR.R
@@ -3,8 +3,7 @@ function (NMcom, b, ProjectDir, boot = 0, concurrent = TRUE,
     SGEflgs = "", checkrunno = TRUE, diag = TRUE, fdata = FALSE, 
     epilog = NULL, dvname = NULL, logtrans = FALSE, covplt = FALSE, 
     grp = NULL, grpnames = NULL, cont.cov = NULL, cat.cov = NULL, 
-    par.list = NULL, eta.list = NULL, missing = -99, dosbox = TRUE, nochecksum = FALSE, grid = FALSE, nice=FALSE,
-    compileonly=FALSE, executeonly=FALSE) 
+    par.list = NULL, eta.list = NULL, missing = -99, dosbox = TRUE, nochecksum = FALSE, grid = FALSE, nice=FALSE) 
 {
     start <- getwd()
     Platform <- "Windows"
@@ -47,8 +46,7 @@ function (NMcom, b, ProjectDir, boot = 0, concurrent = TRUE,
             fdata = fdata, epilog = epilog, dvname = dvname, 
             logtrans = logtrans, covplt = covplt, grp = grp, 
             grpnames = grpnames, cont.cov = cont.cov, cat.cov = cat.cov, 
-            par.list = par.list, eta.list = eta.list, missing = missing, dosbox = dosbox, nochecksum = nochecksum, grid = grid, nice=nice,
-            compileonly = compileonly, executeonly = executeonly)
+            par.list = par.list, eta.list = eta.list, missing = missing, dosbox = dosbox, nochecksum = nochecksum, grid = grid, nice=nice)
         if (!concurrent) 
             do.call("runmsge", run.args)
         if (concurrent & (boot == 1 | boot == 3)) 

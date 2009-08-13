3f6528a39ac3d86245b3185a1e2dbe52bdf19c84
bergsmat <bergsmat@8d4d5234-0756-0410-9cbe-b5ace3ed5f0a>
2009-08-13 23:57:42 +0000

    mods

--- inst/MIfunsExamples/epilogEx.R
+++ inst/MIfunsExamples/epilogEx.R
@@ -1,29 +1,9 @@
 epilog <- function(b,ProjectDir,...){
-    #User code to plot structural/statistical diagnostic plots and/or 
-    #plots to evaluate covariate-parameter relationships and/or
-    #any plots user would like to create with their own code
-	
-    # Important variables that are available for plot script use include:
-	
-    # ProjectDir - Main project directory that contains NONMEM table files (directory referenced in call to NONR)
-    # cont.cov - if defined in NONR
-    # cat.cov - if defined in NONR
-    # par.list - if defined in NONR
-    # eta.list - if defined in NONR
-    # b  - ctl stream number 
-	
-    #Both NONR (runmsge) and PLOTR can call epilog, and both supply everything needed
-    #for dataSynthesis().  dataSynthesis scavenges columns from the TAB file, the par.TAB
-    #file, and the underlying dataset, reporting just observation records that are
-    #not commented.  Limiting to one record per ID is appropriate for some types of plotting.
-    #Columns to scavenge are supplied by grp, cont.cov, cat.cov, par.list, and eta.list.
-    #All TAB file columns are returned, plus first instance of 'scavenge' columns from 
-    #either the parent data set or the par file (in that order) unless already present.
-	
-    #dataSynthesis() returns precisely the dataset that is passed to the standard plots.
+
+    #See R help. dataSynthesis() gives the same dataset that is used by PLOTR.
     data <- dataSynthesis(b=b,ProjectDir=ProjectDir,...)
     
-    numDV<-c('DV','LDV','AMT')  # vector of variable names that should be numeric in data
+    numDV<-c('DV','LDV','AMT') 
     # loop to change variables in numDV to numeric
     for(k in numDV) data[[k]]<-as.numeric(as.character(data[[k]]))
     subj<-data[!duplicated(data$ID),]

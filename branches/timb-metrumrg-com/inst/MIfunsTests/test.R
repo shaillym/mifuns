0e7502cd19c802ea30bc58fc26caf997d203346e
bergsmat <bergsmat@8d4d5234-0756-0410-9cbe-b5ace3ed5f0a>
2009-08-14 23:38:52 +0000

    tests mods

--- branches/timb-metrumrg-com/inst/MIfunsTests/test.R
+++ branches/timb-metrumrg-com/inst/MIfunsTests/test.R
@@ -20,12 +20,14 @@ test <- function(
 	grid=FALSE,
 	NMcom=NULL,
 	b,
+        outdir=paste(ProjectDir,'*',sep='/'),
 	...
 ){
 	InstDir <- absdir(InstDir)
 	NonmemDir <- absdir(NonmemDir)
 	ProjectDir <- absdir(ProjectDir)
 	twig <- twig(NonmemDir)
+        #if(is.null(rundir))rundir <- paste(ProjectDir,'*',sep='/')
 	if(is.null(NMcom)) NMcom <- paste(NonmemDir,'/test/',twig,'.pl',sep='') 
 	infn <- paste(InstDir,'MIfunsExamples/MIfunsRunlogNM6.for',sep='/')
 	misc <- paste(ProjectDir,'0',sep='/')
@@ -50,9 +52,10 @@ test <- function(
 		cat.cov='SEX',
 		par.list=c('CL','V','V2','V3','KA','CLCM','CLCB','CLHC'),
 		eta.list=paste('ETA',1:6,sep=''),
-		file=paste(ProjectDir,'*','diagnostics.pdf',sep='/'),
-		tabfile=paste(ProjectDir,'*','*.TAB',sep='/'),
-		parfile=paste(ProjectDir,'*','*par.TAB',sep='/'),
+		#file=paste(rundir,'diagnostics.pdf',sep='/'),
+		#tabfile=paste(rundir,'*.TAB',sep='/'),
+		#parfile=paste(rundir,'*par.TAB',sep='/'),
+                outdir=outdir,
                 ...
 	)		
 }
@@ -64,24 +67,35 @@ test(InstDir='..',NonmemDir='~/NONMEM/nm6osx1',b=1:2)
 rlog(1:2,out='0/runlog.csv',append=FALSE)
 
 #nix grid
-test(InstDir='..',NonmemDir='/common/NONMEM/nm6osx1',b=1,grid=TRUE,NMcom='nm6osx1.pl')
+InstDir='..'
+NonmemDir='/common/NONMEM/nm6osxbeta'
+test(InstDir,NonmemDir,b=1,grid=FALSE)
+test(InstDir,NonmemDir,b=1:2,grid=FALSE)
+test(InstDir,NonmemDir,b=1,grid=TRUE)
+test(InstDir,NonmemDir,b=1:2,grid=TRUE)
+
+
 data <- read.csv('1.csv',strip.white=TRUE,as.is=TRUE)
-nms <- 1:5
-dir.create('1.boot')
-dir.create('1.boot/0')
-file.copy('0/MIfunsRunlogNM6.for','1.boot/0',overwrite=TRUE)
+nms <- 1001:1005
+
 t <- metaSub(
     as.filename('1.ctl'),
     names=nms,
     pattern=c(
         'RUN# 1',
-        '../1.csv'
+        '../1.csv',
+        '1.TAB',
+        '1par.TAB',
+        '1.MSF'
     ),
     replacement=c(
         'RUN# *',
-        '../*.csv'
+        '../*.csv',
+        '*.TAB',
+        '*par.TAB',
+        '*.MSF'
     ),
-    out='1.boot',
+    out=getwd(),
     suffix='.ctl',
     fixed=TRUE
 )
@@ -90,6 +104,18 @@ r <- resample(
     names=nms,
     key='ID',
     rekey=TRUE,
-    out='1.boot'
+    out=getwd()
 )
-NONR(NMcom='nm6osx1.pl', nms,'1.boot',boot=1,diag=0,grid=TRUE)
+NONR(NMcom='nm6osxbeta.pl',nms,boot=TRUE)
+NONR(NMcom='nm6osxbeta.pl',nms,boot=TRUE,urgent=TRUE)
+bootdir <- paste(getwd(),'*.boot',sep='/')
+test(InstDir,NonmemDir,b=nms,boot=FALSE,concurrent=FALSE,grid=FALSE)#conventional
+test(InstDir,NonmemDir,b=nms,boot=FALSE,concurrent=FALSE,grid=TRUE )#unnecessary chaining
+test(InstDir,NonmemDir,b=nms,boot=FALSE,concurrent=TRUE ,grid=FALSE)#cross-chatter on stdout
+test(InstDir,NonmemDir,b=nms,boot=FALSE,concurrent=TRUE ,grid=TRUE )#conventional grid
+test(InstDir,NonmemDir,b=nms,boot=TRUE ,concurrent=FALSE,grid=FALSE,outdir=bootdir)#boot-style directories
+test(InstDir,NonmemDir,b=nms,boot=TRUE ,concurrent=FALSE,grid=TRUE )#chained boots, no plotting (outdir irrelevant)
+test(InstDir,NonmemDir,b=nms,boot=TRUE ,concurrent=TRUE ,grid=FALSE,outdir=bootdir)#concurrent non-grid boots (chatter)
+test(InstDir,NonmemDir,b=nms,boot=TRUE ,concurrent=TRUE ,grid=TRUE )#conventional boots
+system('qstat')
+rlog(nms,boot=TRUE,append=FALSE,out='boots.log')

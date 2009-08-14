6d4c227eacc508d1b699523deab74ca7a6bc136c
bergsmat <bergsmat@8d4d5234-0756-0410-9cbe-b5ace3ed5f0a>
2009-08-14 23:42:16 +0000

    default form

--- inst/MIfunsTests/0/MIfunsRunlogNM6.for
+++ inst/MIfunsTests/0/MIfunsRunlogNM6.for
@@ -9,7 +9,7 @@ C Generates a running table file to summarize the output of NONMEM runs.
       end
 	  
       subroutine runlog(NEWIND)
-      include '/common/NONMEM/nm6osxbeta/SIZES'
+      include 'SIZES'
       common /cm1/ ntheta,nth
       common /cm2/ neta,neps,nre2
 	  common /cm34/ ie

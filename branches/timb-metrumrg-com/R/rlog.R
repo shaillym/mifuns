368bed3dd29dd175ea4fec7d102a865910d9e7b3
bergsmat <bergsmat@8d4d5234-0756-0410-9cbe-b5ace3ed5f0a>
2009-08-14 19:16:44 +0000

    candidate for windows testing

--- branches/timb-metrumrg-com/R/rlog.R
+++ branches/timb-metrumrg-com/R/rlog.R
@@ -1,16 +1,15 @@
-`rlog` <-
-  function (
+`rlog` <-function(
   	b, 
 	boot=FALSE, 
 	ProjectDir=getwd(), 
 	append=TRUE,
 	file=NULL,
-	out=NULL,
+	out=filename(ProjectDir,'CombRunLog.csv'),
+	pattern=if(boot)'/FD*'else c('/F*','/nonmem.exe','/P*','/O*','/Run*'),
 	...
 ){
-  #if(win()) boot <- FALSE
-  if(is.null(out)) out <- paste(ProjectDir,'CombRunLog.csv',sep='/')
   if(!append) if(file.exists(out)) file.remove(out)
+  b <- unique(b)
   for(i in b){
       #identify objects
       rdir <- paste(ProjectDir, '/', i, sep = '')    
@@ -18,14 +17,7 @@
       if(is.null(file)) f <- paste(rdir,'NonmemRunLog.csv',sep='/')
       f <- star(f,i)
       #cleanup
-      if (nix()) if(!boot) purge.files('/FD*',rdir)
-      if (nix()) if( boot) if (!file.exists(paste(rdir, '/FILE10', sep = ''))) {
-          purge.files('/F*',rdir)
-          purge.files('/nonmem.exe',rdir)
-          purge.files('/P*', rdir)
-          purge.files('/O*',rdir)
-          purge.files('/Run*',rdir)
-      } 
+      lapply(pattern,purge.files,rdir)
       #append log
       if(!file.exists(f)) cat(paste('Log for Run ', i, ' does not exist', '\n', sep = ''))
       else {

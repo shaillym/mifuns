a7572f021583fe9e36ecfec2e2fff7bd5120d429
bergsmat <bergsmat@8d4d5234-0756-0410-9cbe-b5ace3ed5f0a>
2010-01-05 17:55:08 +0000

    as.xml

--- trunk/R/toXML.R
+++ trunk/R/toXML.R
@@ -1,20 +1,20 @@
-`toXML` <-
-function(x,...)UseMethod('toXML')
-`toXML.character` <-
+`as.xml` <-
+function(x,...)UseMethod('as.xml')
+`as.xml.character` <-
 function(x,tag,...)paste(
 	sep='',
 	bracket(tag,...),
 	x,
 	bracket(tag,close=TRUE)
 )
-`toXML.data.frame` <-
+`as.xml.data.frame` <-
 function(x,keyname='row',keys=rownames(x),...){
 	itemize <- function(col,frame,tag,key){
 		values <- frame[,col]
-		nest(toXML(values,tag=tag,key=key),tag=col)
+		nest(as.xml(values,tag=tag,key=key),tag=col)
 	}
 	unlist(lapply(names(x),itemize,frame=x,tag=keyname,key=keys))
 }
-`toXML.default` <-
-function(x,tag,...)toXML(as.character(x),tag,...)
+`as.xml.default` <-
+function(x,tag,...)as.xml(as.character(x),tag,...)
 

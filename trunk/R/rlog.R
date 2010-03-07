61cf27a61c788a799f28f2a9ef43135004b9b24e
bergsmat <bergsmat@8d4d5234-0756-0410-9cbe-b5ace3ed5f0a>
2010-03-07 04:31:21 +0000

    general improvements

--- trunk/R/rlog.R
+++ trunk/R/rlog.R
@@ -6,24 +6,29 @@
 	file=NULL,
 	out=filename(project,'CombRunLog.csv'),
 	pattern=if(boot)c('^F','^nonmem.exe','^P','^O','^Run') else '^FD',
+	software='nm6',
 	...
 ){
   if(!append) if(file.exists(out)) file.remove(out)
   run <- unique(run)
   for(each in run){
       #identify objects
-      rdir <- filename(project, each, if(boot) '.boot' else '')
-      if(is.null(file)) f <- file.path(rdir,'NonmemRunLog.csv')
+      rundir <- filename(project, each, if(boot) '.boot' else '')
+      outfile = filename(rundir,run,'.lst')
+      if(is.null(file)) f <- file.path(rundir,'NonmemRunLog.csv')
       f <- star(f,each)
       #cleanup
-      lapply(pattern,purge.files,rdir)
+      lapply(pattern,purge.files,rundir)
       #append log
+      if(software=='nm7')runlog(run=run,outfile=outfile,...)
       if(!file.exists(f)) cat(paste('Log for Run ', each, ' does not exist', '\n', sep = ''))
       else {
 	  dat <- readLines(f)
 	  dat <- dat[nchar(dat)>0]
           cat(dat,sep='\n',file=out,append=TRUE)
       }
-  }
+   }
+   if(software=='nm6')return()
+   if(software=='nm7')invisible(do.call(rbind,lapply(run,unilog,boot=boot,project=project,...)))
  }
 

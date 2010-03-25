534c10fd8380e25ba413ceb85dbcc0425f1d7669
bergsmat <bergsmat@8d4d5234-0756-0410-9cbe-b5ace3ed5f0a>
2010-03-25 15:20:02 +0000

    cleaner examples

--- R/ibw.R
+++ R/ibw.R
@@ -17,12 +17,12 @@ function(wt,ht)signif(exp(-3.751) * ht^0.422 * wt^0.515,3)
 function(age,wt,male,scr){
 clearance <- (140-age)*wt/(72*scr)#scr: mg/dL
 clearance[!male] <- clearance[!male] * 0.85
-return(clearance)
+return(signif(clearance,3))
 }
 `lbm` <-
 function(wt,ht,male){
-males <- 1.10 * wt - 128 * (wt^2/ht^2)
-females <- 1.07 * wt - 148 * (wt^2/ht^2)
+males <- signif(1.10 * wt - 128 * (wt^2/ht^2),3)
+females <- signif(1.07 * wt - 148 * (wt^2/ht^2),3)
 ifelse(male,males,females)
 #http://www.halls.md/body-mass-index/leanbody.htm
 #Lean Body Weight (men) = (1.10 x Weight(kg)) - 128 x ( Weight2/(100 x Height(m))2)

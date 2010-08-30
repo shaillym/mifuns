9f2af07a0f9009a499bd3a935dcca4a045965073
bergsmat <bergsmat@8d4d5234-0756-0410-9cbe-b5ace3ed5f0a>
2010-08-30 19:31:57 +0000

    attempt at fixing the setClass bug

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,14 @@
 .onAttach <-function(lib,pkg)
 {
+    setClass(
+    	"nm.data", 
+    	representation(
+    		data = "data.frame", 
+    		eta = "data.frame", 
+    		theta = "data.frame", 
+    		omega = "data.frame", 
+    		sigma = "data.frame"
+    	)
+    )
     cat('MIfuns',utils::packageDescription('MIfuns',fields='Version'),'\n')
 }

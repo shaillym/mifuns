55ef8a7bc67584773a07401f0fa39528ae76ae74
bergsmat <bergsmat@8d4d5234-0756-0410-9cbe-b5ace3ed5f0a>
2010-08-30 17:42:37 +0000

    namespace debugged

--- trunk/R/zzz.R
+++ trunk/R/zzz.R
@@ -1,14 +1,4 @@
 .onAttach <-function(lib,pkg)
 {
-    ver <- as.character(
-    	read.dcf(
-		file.path(
-			lib, 
-			pkg, 
-			"DESCRIPTION"
-		), 
-		"Version"
-	)
-    )
-    paste("MIfuns", ver, "loaded\n")
+    cat('MIfuns',packageDescription('MIfuns',fields='Version'),'\n')
 }

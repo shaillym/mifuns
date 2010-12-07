aecc912123fe18402d76aa81ce42e9aeb3420697
bergsmat <bergsmat@8d4d5234-0756-0410-9cbe-b5ace3ed5f0a>
2010-12-07 19:27:58 +0000

    4.1.9

--- code/R/runlog.R
+++ code/R/runlog.R
@@ -3,7 +3,10 @@ as.pxml.ext <- function(file,lead=1,tag='param',...){
 	x <- readLines(file)
 	head <- seq(length.out=lead)
 	note <- as.xml(x[head],tag='note')
-	body <- as.data.frame.block(x[-head])
+	body <- tryCatch(
+		as.data.frame.block(x[-head]),
+		error=function(e)stop('cannot process ',file,call.=FALSE)
+	)
 	body <- as.xml(body[,-1],keyname=names(body)[[1]], key=body[[1]])
 	nest(tag='param',x=c(note,body))
 }

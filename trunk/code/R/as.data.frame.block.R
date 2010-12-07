8102b1cd97d4e5debe8cb68773281aeecc0a75a5
bergsmat <bergsmat@8d4d5234-0756-0410-9cbe-b5ace3ed5f0a>
2010-12-07 19:14:44 +0000

    4.1.9

--- trunk/code/R/as.data.frame.block.R
+++ trunk/code/R/as.data.frame.block.R
@@ -3,6 +3,7 @@ function(x,...){
 	con <- textConnection(x)
 	dat <- tryCatch(
 		read.table(con,header=TRUE,as.is=TRUE,check.names=FALSE,...),
+		error=function(e)stop('cannot process ',x,call.=FALSE),
 		finally=close(con)
 	)
 	dat

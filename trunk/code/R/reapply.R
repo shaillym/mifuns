8d08fd4000d76f3e09f4aa79af0c3b39331457d7
bergsmat <bergsmat@8d4d5234-0756-0410-9cbe-b5ace3ed5f0a>
2011-01-20 02:56:17 +0000

    bug in reapply

--- trunk/code/R/reapply.R
+++ trunk/code/R/reapply.R
@@ -1,21 +1,23 @@
 reapply <- 
 function (x, INDEX, FUN, ...) 
 {
-    if(!is.list(INDEX)) INDEX <- list(INDEX)
-    INDEX <- lapply(INDEX,function(x)as.integer(factor(x)))
-    INDEX <- as.integer(do.call(interaction,c(INDEX,drop=TRUE)))
-    form <- tapply(x, INDEX)
-    calc <- tapply(x, INDEX, FUN, ...,simplify=FALSE)
-    need <- table(form)
-    calc <- lapply(
-    	seq_along(calc),
-    	function(cell)rep(
-    		calc[[cell]],
-    		length.out=need[[
-    			as.character(cell)
-    		]]
-    	)
-    )
+	if(!is.list(INDEX)) INDEX <- list(INDEX)
+	INDEX <- lapply(INDEX,function(x)as.integer(factor(x)))
+	INDEX <- as.integer(do.call(interaction,c(INDEX,drop=TRUE)))
+	form <- tapply(x, INDEX)
+	calc <- tapply(x, INDEX, FUN, ...,simplify=FALSE)
+	need <- table(form)
+	calc <- lapply(
+		seq_along(calc),
+		function(cell)rep(
+			calc[[cell]],
+			length.out=need[[
+				as.character(cell)
+			]]
+		)
+	)
+	calc <- c(calc,list(rep(NA,sum(is.na(form)))))
+	form[is.na(form)] <- length(calc)
 	grps <- split(form,form)
 	grps <- lapply(
 		grps, 
@@ -24,7 +26,7 @@ function (x, INDEX, FUN, ...)
 	elem <- unsplit(grps,form)
 	sapply(
 		seq_along(form),
-		function(i)if(is.na(form[[i]])) NA else calc[[
+		function(i)calc[[
 			form[[i]]
 		]][[
 			elem[[i]]
